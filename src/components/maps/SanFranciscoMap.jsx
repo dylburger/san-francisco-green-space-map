c501c8863ed6823dd7ac6b80558b3c0e616f8856
Dylan Sather <dylan.sather@gmail.com>
2018-01-14 11:15:50 -0800

    removing GeoJSON green space layer, since Mapbox GL failed to render the polygons correctly at a low zoom.
    Adding pitch and bearing to the map.

--- src/components/maps/SanFranciscoMap.jsx
+++ src/components/maps/SanFranciscoMap.jsx
@@ -2,16 +2,6 @@ import React from 'react';
 import mapboxgl from 'mapbox-gl';
 
 import config from 'config';
-import data from 'geojson/sf_rec_parks_properties.json';
-
-const options = [
-  {
-    name: 'Parks',
-    description: 'Parks',
-    property: 'parks',
-    fillColor: '#1e932d',
-  },
-];
 
 class SanFranciscoMap extends React.Component {
   constructor(props) {
@@ -28,7 +18,6 @@ class SanFranciscoMap extends React.Component {
     } = config.maps.sanFrancisco;
 
     this.state = {
-      active: options[0],
       latitude,
       longitude,
       zoom,
@@ -44,46 +33,21 @@ class SanFranciscoMap extends React.Component {
   }
 
   componentDidMount() {
-    const {latitude, longitude, zoom} = this.state;
+    const {latitude, longitude, zoom, pitch, bearing} = this.state;
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/dylburger/cjccuciq62k5o2rqw7ha6gp1p',
+      style: 'mapbox://styles/mapbox/light-v9',
       center: [longitude, latitude],
       zoom,
+      pitch,
+      bearing,
     });
-
-    this.map.on('load', () => {
-      this.map.addSource('parks', {
-        type: 'geojson',
-        data,
-      });
-
-      this.map.addLayer(
-        {
-          id: 'parks',
-          type: 'fill',
-          source: 'parks',
-        },
-        'country-label-lg',
-      ); // ID matches `mapbox/streets-v9`
-
-      this.setFill();
-    });
-  }
-
-  componentDidUpdate() {
-    this.setFill();
-  }
-
-  setFill() {
-    const {fillColor} = this.state.active;
-    this.map.setPaintProperty('parks', 'fill-color', fillColor);
   }
 
   render() {
     return (
       <div
-        ref={el => (this.mapContainer = el)}
+        ref={el => (this.mapContainer = el)} // eslint-disable-line no-return-assign
         className="absolute top right left bottom"
       />
     );
