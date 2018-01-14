e186bc52beb1487bcf91671b7329dc5c0b6f02ca
Dylan Sather <dylan.sather@gmail.com>
2018-01-14 12:52:02 -0800

    Filtering Camp Mather from analysis

--- scripts/fetch_parks_metadata.py
+++ scripts/fetch_parks_metadata.py
@@ -5,9 +5,19 @@ DATA_SF_PARKS_JSON_URL = 'https://data.sfgov.org/resource/94uf-amnx.json'
 JSON_OUTFILE = '../public/data/sf_parks_metadata.json'
 
 print("Fetching Parks JSON file from %s" % DATA_SF_PARKS_JSON_URL)
-data = requests.get(DATA_SF_PARKS_JSON_URL).json()
+parks = requests.get(DATA_SF_PARKS_JSON_URL).json()
+
+
+def filter_camp_mather(park):
+    if park.get('parkname') != 'CAMP MATHER':
+        return park
+    else:
+        return {'parkname': 'CAMP MATHER'}
+
+
+filtered_park_data = list(map(filter_camp_mather, parks))
 
 with open(JSON_OUTFILE, 'w') as outfile:
-    json.dump(data, outfile)
+    json.dump(filtered_park_data, outfile)
 
 print("Saved parks JSON in %s" % (JSON_OUTFILE))
