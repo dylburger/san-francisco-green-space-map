4d2762a4dada10675d88221af32c9db288eb97c0
Dylan Sather <dylan.sather@gmail.com>
2018-01-14 11:28:38 -0800

    Adding 3D building layer

--- src/components/maps/SanFranciscoMap.jsx
+++ src/components/maps/SanFranciscoMap.jsx
@@ -42,6 +42,58 @@ class SanFranciscoMap extends React.Component {
       pitch,
       bearing,
     });
+
+    // Thanks to the Mapbox docs for this example of loading 3D building data.
+    // https://www.mapbox.com/mapbox-gl-js/example/3d-buildings/
+    this.map.on('load', () => {
+      // Insert the layer beneath any symbol layer.
+      const layers = this.map.getStyle().layers;
+
+      let labelLayerId;
+      for (let i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+          labelLayerId = layers[i].id;
+          break;
+        }
+      }
+
+      this.map.addLayer(
+        {
+          id: '3d-buildings',
+          source: 'composite',
+          'source-layer': 'building',
+          filter: ['==', 'extrude', 'true'],
+          type: 'fill-extrusion',
+          minzoom: 15,
+          paint: {
+            'fill-extrusion-color': '#aaa',
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height'],
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height'],
+            ],
+            'fill-extrusion-opacity': 0.6,
+          },
+        },
+        labelLayerId,
+      );
+    });
   }
 
   render() {
