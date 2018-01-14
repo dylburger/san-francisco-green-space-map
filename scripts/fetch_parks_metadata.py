42a41fddbf837daf121762d8017bbc4cace0276e
Dylan Sather <dylan.sather@gmail.com>
2018-01-14 12:57:24 -0800

    Refactoring parks metadata script to filter out Camp Mather

--- scripts/fetch_parks_metadata.py
+++ scripts/fetch_parks_metadata.py
@@ -4,18 +4,21 @@ import requests
 DATA_SF_PARKS_JSON_URL = 'https://data.sfgov.org/resource/94uf-amnx.json'
 JSON_OUTFILE = '../public/data/sf_parks_metadata.json'
 
-print("Fetching Parks JSON file from %s" % DATA_SF_PARKS_JSON_URL)
-parks = requests.get(DATA_SF_PARKS_JSON_URL).json()
-
 
 def filter_camp_mather(park):
-    if park.get('parkname') != 'CAMP MATHER':
-        return park
-    else:
-        return {'parkname': 'CAMP MATHER'}
+    """ We do not want to include Camp Mather in our dataset, since
+        it's outside of the SF city limits.
+    """
+    return park.get('parkname') != 'CAMP MATHER'
 
 
-filtered_park_data = list(map(filter_camp_mather, parks))
+def filter_park(park):
+    return filter_camp_mather(park)
+
+
+print("Fetching Parks JSON file from %s" % DATA_SF_PARKS_JSON_URL)
+parks = requests.get(DATA_SF_PARKS_JSON_URL).json()
+filtered_park_data = list(filter(filter_park, parks))
 
 with open(JSON_OUTFILE, 'w') as outfile:
     json.dump(filtered_park_data, outfile)
